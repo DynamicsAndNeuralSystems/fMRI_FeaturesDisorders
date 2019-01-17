f1dbeb55c2f4280bb19b43cca7e55ea0064c953b
Adithya Vignaraja <avig6779@uni.sydney.edu.au>
2019-01-17 13:56:02 +1100

    Removed the older version of classifyFeatMat.ipynb (featuresAnalysis.ipynb) Renamed a file - classifyFeatMat.py Added a new file - catch22_4_matFiles.py

---
+++ catch22_4_matFiles.py
@@ -0,0 +1,90 @@
+import glob
+import scipy.io as sio
+import catch22
+import numpy as np
+
+path = '/Users/AV/Dropbox/UCLA/'
+
+# This will arrange the assigned file type in ALPHABETICAL ORDER
+TS_path_names = sorted(glob.glob(path + '*.mat'))
+
+#-------------------------------------------------------------------------------
+''' This function extracts data from any element of 'roiTS' within the cfg
+    structure in the .mat file '''
+
+def gettsData(path,element):
+    matFile = sio.loadmat(path)
+    tsData = matFile['cfg'][0][0]['roiTS'][0][element]
+    return tsData
+#-------------------------------------------------------------------------------
+''' This function returns a catch-22 feature vector from an input time-series
+    list '''
+
+def giveMeFeatureVector(tsData):
+
+    features = dir(catch22)
+    features = [item for item in features if not '__' in item]
+
+    featureVector = []
+    for testFun in features:
+        featureFun = getattr(catch22,testFun)
+        featureVector.append(featureFun(tsData))
+
+    return featureVector
+#-------------------------------------------------------------------------------
+''' This function takes any element of the 'roiTS' cell from each of the
+    subject data files and an input ROI (a specified column of the matrix),
+    converts these columns into a list, processes them using the catch22 module
+    and stores them in a variable that is returned, smallFeatMat '''
+
+def giveMeSmallFeatMat(element,roi):
+
+    # Initialise indices
+    i = 0
+
+    for path in TS_path_names:
+
+        # Retrieve and store data in a temp variable
+        tsData = gettsData(path,element)
+
+        # Need to initialise the smallFeatMat for the first iteration of the loop
+        if i == 0:
+
+            smallFeatMat = np.zeros(shape=(len(TS_path_names),22))
+
+        # Get a specific column from the ts x roi data matrix for all the files
+        tsDataCol = tsData[:,roi]
+
+        # To be recognised by the catch22 modules, the input array needs to be a list
+        tsDataCol = tsDataCol.tolist()
+        smallFeatMat[i,:] = giveMeFeatureVector(tsDataCol)
+
+        # Increment index
+        i += 1
+
+    return smallFeatMat
+#-------------------------------------------------------------------------------
+''' Enter function description here '''
+
+def giveMeBigFeatMat(element):
+
+    # Assuming each of the data files have an indentical structure, use the first
+    # element's data as a template to initialise a few variables
+    [rows, cols] = gettsData(TS_path_names[0],element).shape
+
+    for n in range(cols):
+
+        if n == 0:
+            bigFeatMat = giveMeSmallFeatMat(element,n)
+
+        else:
+            smallFeatMat = giveMeSmallFeatMat(element,n)
+            bigFeatMat = np.vstack([bigFeatMat,smallFeatMat])
+
+    return bigFeatMat
+#-------------------------------------------------------------------------------
+
+# Testing...
+print(giveMeBigFeatMat(2).shape)
+
+# np.savetxt('featureMatrixPy.txt',x,delimiter=',')
