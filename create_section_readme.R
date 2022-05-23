3cabe15e6449fbc9213e8924d148e7c8b5732e11
Annie G. Bryant <anniegbryant@gmail.com>
2022-05-23 15:04:43 +1000

    readme

--- create_section_readme.R
+++ create_section_readme.R
@@ -1,16 +1,25 @@
+github_dir <- "D:/Virtual_Machines/Shared_Folder/github/fMRI_FeaturesDisorders/"
+setwd(github_dir)
+
 # Step 1
-setwd("D:/Virtual_Machines/Shared_Folder/github/fMRI_FeaturesDisorders")
+
 # Step 2
 
 # Step 3
-rmarkdown::render("3_ROI_wise_analysis/STEP3_README.Rmd", output_file = "D:/Virtual_Machines/Shared_Folder/github/fMRI_FeaturesDisorders/3_ROI_wise_analysis/README.md")
-
+rmarkdown::render(paste0(github_dir,"3_ROI_wise_analysis/STEP3_README.Rmd"))
+file.copy(paste0(github_dir,"3_ROI_wise_analysis/STEP3_README.md"),
+          paste0(github_dir,"3_ROI_wise_analysis/README.md"),
+          overwrite = T)
 # Step 4
-rmarkdown::render("4_feature_wise_analysis/STEP4_README.Rmd", output_file = "D:/Virtual_Machines/Shared_Folder/github/fMRI_FeaturesDisorders/4_feature_wise_analysis/README.md")
-
+rmarkdown::render(paste0(github_dir,"4_Feature_wise_analysis/STEP4_README.Rmd"))
+file.copy(paste0(github_dir,"4_Feature_wise_analysis/STEP4_README.md"),
+          paste0(github_dir,"4_Feature_wise_analysis/README.md"),
+          overwrite = T)
 # Step 5
-rmarkdown::render("5_ROI_and_feature_wise_analysis/STEP5_README.Rmd", output_file = "D:/Virtual_Machines/Shared_Folder/github/fMRI_FeaturesDisorders/5_ROI_and_feature_wise_analysis/README.md")
-
+rmarkdown::render(paste0(github_dir,"5_ROI_and_feature_wise_analysis/STEP5_README.Rmd"))
+file.copy(paste0(github_dir,"5_ROI_and_feature_wise_analysis/STEP5_README.md"),
+          paste0(github_dir,"5_ROI_and_feature_wise_analysis/README.md"),
+          overwrite = T)
 # Step 6
 
 # Step 7
