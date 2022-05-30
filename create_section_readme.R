2dcd0043b1ccfcf7995f2c6879716793a7de9bd1
Annie G. Bryant <anniegbryant@gmail.com>
2022-05-30 11:26:16 +1000

    misc org

--- create_section_readme.R
+++ create_section_readme.R
@@ -6,18 +6,18 @@ setwd(github_dir)
 # Step 2
 
 # Step 3
-rmarkdown::render(paste0(github_dir,"3_ROI_wise_analysis/STEP3_README.Rmd"))
-file.copy(paste0(github_dir,"3_ROI_wise_analysis/STEP3_README.md"),
+rmarkdown::render(paste0(github_dir,"3_ROI_wise_analysis/Step3_README.Rmd"))
+file.copy(paste0(github_dir,"3_ROI_wise_analysis/Step3_README.md"),
           paste0(github_dir,"3_ROI_wise_analysis/README.md"),
           overwrite = T)
 # Step 4
-rmarkdown::render(paste0(github_dir,"4_Feature_wise_analysis/STEP4_README.Rmd"))
-file.copy(paste0(github_dir,"4_Feature_wise_analysis/STEP4_README.md"),
+rmarkdown::render(paste0(github_dir,"4_Feature_wise_analysis/Step4_README.Rmd"))
+file.copy(paste0(github_dir,"4_Feature_wise_analysis/Step4_README.md"),
           paste0(github_dir,"4_Feature_wise_analysis/README.md"),
           overwrite = T)
 # Step 5
-rmarkdown::render(paste0(github_dir,"5_ROI_and_feature_wise_analysis/STEP5_README.Rmd"))
-file.copy(paste0(github_dir,"5_ROI_and_feature_wise_analysis/STEP5_README.md"),
+rmarkdown::render(paste0(github_dir,"5_ROI_and_feature_wise_analysis/Step5_README.Rmd"))
+file.copy(paste0(github_dir,"5_ROI_and_feature_wise_analysis/Step5_README.md"),
           paste0(github_dir,"5_ROI_and_feature_wise_analysis/README.md"),
           overwrite = T)
 # Step 6
