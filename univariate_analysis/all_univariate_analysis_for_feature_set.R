4d9b6d6c3e28ed5abbd6f0a7221497a6a325925e
Annie G. Bryant <anniegbryant@gmail.com>
2022-07-06 10:00:38 +1000

    Update all_univariate_analysis_for_feature_set.R

--- univariate_analysis/all_univariate_analysis_for_feature_set.R
+++ univariate_analysis/all_univariate_analysis_for_feature_set.R
@@ -2,6 +2,7 @@
 library(argparse)
 parser <- ArgumentParser(description = "Define data paths and feature set")
 
+parser$add_argument("--project_path", default="/project/hctsa/annie/")
 parser$add_argument("--github_dir", default="/project/hctsa/annie/github/")
 parser$add_argument("--rdata_path", default="/project/hctsa/annie/data/scz/UCLA/Rdata/")
 parser$add_argument("--feature_set", default="catch22")
@@ -11,6 +12,7 @@ parser$add_argument("--feature_set", default="catch22")
 
 # Parse input arguments
 args <- parser$parse_args()
+project_path <- args$project_path
 github_dir <- args$github_dir
 rdata_path <- args$rdata_path
 feature_set <- args$feature_set
