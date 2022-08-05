00503c945ee5b90b6ea905f6f33b420c776645ff
Annie G. Bryant <anniegbryant@gmail.com>
2022-08-05 13:36:07 +1000

    Update run_pyspi_distribute.sh

--- data_prep_and_QC/run_pyspi_distribute.sh
+++ data_prep_and_QC/run_pyspi_distribute.sh
@@ -18,14 +18,14 @@ done
 
 
 # ABIDE ASD
-# dataset_ID="ABIDE_ASD"
-# for noise_label in FC1000
-# do
-#     python $pyspi_script_dir/distribute_jobs.py --data_dir ${study}/data/${dataset_ID}/pydata/${noise_label}/ \
-#     --compute_file $pyspi_script_dir/pyspi_compute.py \
-#     --template_pbs_file $pyspi_script_dir/template.pbs \
-#     --pyspi_config $config_file \
-#     --sample_yaml ${study}/data/${dataset_ID}/pydata/${noise_label}/sample.yaml \
-#     --pbs_notify a \
-#     --email abry4213@uni.sydney.edu.au --walltime_hrs 6 --cpu 2 --mem 8 --table_only
-# done
+dataset_ID="ABIDE_ASD"
+for noise_label in FC1000
+do
+    python $pyspi_script_dir/distribute_jobs.py --data_dir ${study}/data/${dataset_ID}/pydata/${noise_label}/ \
+    --compute_file $pyspi_script_dir/pyspi_compute.py \
+    --template_pbs_file $pyspi_script_dir/template.pbs \
+    --pyspi_config $config_file \
+    --sample_yaml ${study}/data/${dataset_ID}/pydata/${noise_label}/sample_onesubj.yaml \
+    --pbs_notify abe \
+    --email abry4213@uni.sydney.edu.au --walltime_hrs 6 --cpu 2 --mem 8 --table_only
+done
