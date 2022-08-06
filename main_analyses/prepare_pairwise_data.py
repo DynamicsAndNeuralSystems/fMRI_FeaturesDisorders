15a91253b4bd3b8dc86b1cbd2973bdeaa0e30383
Annie G. Bryant <anniegbryant@gmail.com>
2022-08-06 15:26:07 +1000

    Update prepare_pairwise_data.py

--- main_analyses/prepare_pairwise_data.py
+++ main_analyses/prepare_pairwise_data.py
@@ -14,7 +14,7 @@ parser.add_argument('--data_path', default="/project/hctsa/annie/data/UCLA_Schiz
 parser.add_argument('--input_mat_file', default="", nargs="?", dest='input_mat_file')
 parser.add_argument('--subject_csv', default="participants.csv", dest='subject_csv')
 parser.add_argument('--pairwise_feature_set', default="pyspi19", dest='pairwise_feature_set')
-parser.add_argument('--pyspi_config_file', dest='pairwise_feature_set')
+parser.add_argument('--pyspi_config_file', dest='pyspi_config_file')
 parser.add_argument('--parcellation_name', default="harvard_oxford_cort_prob_2mm", dest='parcellation_name', nargs='?')
 parser.add_argument('--brain_region_lookup', default="Harvard_Oxford_cort_prob_2mm_ROI_lookup.csv", dest='brain_region_lookup', nargs='?')
 parser.add_argument('--noise_procs', default=["AROMA+2P", "AROMA+2P+GMR", "AROMA+2P+DiCER"], nargs='*', dest='noise_procs')
