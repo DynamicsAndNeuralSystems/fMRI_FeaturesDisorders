f930bad2f8066377e53b133985132f0f80492ff1
Annie G. Bryant <anniegbryant@gmail.com>
2022-08-06 16:46:28 +1000

    Update run_pyspi_distribute_ABIDE_ASD.sh

--- data_prep_and_QC/run_pyspi_distribute_ABIDE_ASD.sh
+++ data_prep_and_QC/run_pyspi_distribute_ABIDE_ASD.sh
@@ -19,5 +19,5 @@ do
     --pyspi_config $config_file \
     --sample_yaml ${project_dir}/data/${dataset_ID}/pydata/${noise_label}/sample_onesubj.yaml \
     --pbs_notify a \
-    --email $email --walltime_hrs 1 --cpu 2 --mem 8 --table_only
+    --email $email --walltime_hrs 1 --cpu 2 --mem 8 --table_only --overwrite
 done
