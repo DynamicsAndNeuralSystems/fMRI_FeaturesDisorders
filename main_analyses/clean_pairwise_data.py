1507293a46ebaebd4dadf08c1b60af6d0f4995a3
Annie G. Bryant <anniegbryant@gmail.com>
2022-08-07 19:46:09 +1000

    reorganise pyspi distribute scripts

--- main_analyses/clean_pairwise_data.py
+++
@@ -1,3 +0,0 @@
-# Write calc.table pkl files to CSVs
-pkl_to_csv_cmd=f"python3 {github_dir}/fMRI_FeaturesDisorders/data_prep_and_QC/pyspi_pickle_to_csv --github_dir {github_dir} --data_path {data_path} --noise_procs {noise_procs_cl} --brain_region_lookup {brain_region_lookup} --parcellation_name {parcellation_name} --dataset_ID {dataset_ID}"
-os.system(pkl_to_csv_cmd)  
