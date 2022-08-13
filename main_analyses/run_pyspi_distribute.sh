1f43a05aed34f6c5101257047a2dc0b363ba2473
Annie G. Bryant <anniegbryant@gmail.com>
2022-08-13 17:31:53 +1000

    Update run_pyspi_distribute.sh

--- main_analyses/run_pyspi_distribute.sh
+++ main_analyses/run_pyspi_distribute.sh
@@ -6,17 +6,17 @@ export pyspi_script_dir=${project_dir}/github/pyspi-distribute/
 export email="abry4213@uni.sydney.edu.au"
 
 # UCLA Schizophrenia
-# dataset_ID="UCLA_Schizophrenia"
-# for noise_label in AROMA_2P AROMA_2P_GMR AROMA_2P_DiCER
-# do
-#     python $pyspi_script_dir/distribute_jobs.py --data_dir ${project_dir}/data/${dataset_ID}/pydata/${noise_label}/ \
-#     --compute_file $pyspi_script_dir/pyspi_compute.py \
-#     --template_pbs_file $pyspi_script_dir/template.pbs \
-#     --pyspi_config $config_file \
-#     --sample_yaml ${project_dir}/data/${dataset_ID}/pydata/${noise_label}/sample.yaml \
-#     --pbs_notify a \
-#     --email $email --walltime_hrs 8 --cpu 1 --mem 20 --table_only
-# done
+dataset_ID="UCLA_Schizophrenia"
+for noise_label in AROMA_2P AROMA_2P_GMR AROMA_2P_DiCER
+do
+    python $pyspi_script_dir/distribute_jobs.py --data_dir ${project_dir}/data/${dataset_ID}/pydata/${noise_label}/ \
+    --compute_file $pyspi_script_dir/pyspi_compute.py \
+    --template_pbs_file $pyspi_script_dir/template.pbs \
+    --pyspi_config $config_file \
+    --sample_yaml ${project_dir}/data/${dataset_ID}/pydata/${noise_label}/sample.yaml \
+    --pbs_notify a \
+    --email $email --walltime_hrs 8 --cpu 1 --mem 20 --table_only
+done
 
 
 # ABIDE ASD
