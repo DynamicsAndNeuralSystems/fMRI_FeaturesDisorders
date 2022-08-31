620eeb83fd72318f9ebd7eb7b7d07a0a6a95e78c
Annie G. Bryant <anniegbryant@gmail.com>
2022-08-31 21:16:24 +1000

    Update prepare_pairwise_data.sh

--- data_prep_and_QC/prepare_pairwise_data.sh
+++ data_prep_and_QC/prepare_pairwise_data.sh
@@ -22,16 +22,16 @@ cmd="python3 ${fmri_github_dir}/helper_functions/data_prep_and_QC/split_MTS_into
 echo $cmd
 $cmd
 
-# # Create sample.yaml file for this dataset
-# for noise_proc in $noise_procs_list
-# do
-#     noise_label=$(echo $noise_proc | sed "s/\+/_/g")
-#     cmd="Rscript $github_dir/pyspi-distribute/create_yaml_for_samples.R \
-#     --data_dir ${data_path}/raw_data/numpy_files/${noise_label}/ \
-#     --sample_metadata_file ${data_path}/${sample_metadata_file} \
-#     --ID_var Sample_ID \
-#     --label_vars Diagnosis \
-#     --dim_order ps \
-#     --overwrite"
-#     $cmd
-# done
+# Create sample.yaml file for this dataset
+for noise_proc in $noise_procs_list
+do
+    noise_label=$(echo $noise_proc | sed "s/\+/_/g")
+    cmd="Rscript $github_dir/pyspi-distribute/create_yaml_for_samples.R \
+    --data_dir ${data_path}/raw_data/numpy_files/${noise_label}/ \
+    --sample_metadata_file ${data_path}/${sample_metadata_file} \
+    --ID_var Sample_ID \
+    --label_vars Diagnosis \
+    --dim_order ps \
+    --overwrite"
+    $cmd
+done
