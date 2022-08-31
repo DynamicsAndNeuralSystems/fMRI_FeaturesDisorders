3a2900665ccfd9c1edb7ece46c5fe9ca2b9aab9e
Annie G. Bryant <anniegbryant@gmail.com>
2022-08-31 21:11:36 +1000

    splitting mts

--- data_prep_and_QC/prepare_pairwise_data.sh
+++ data_prep_and_QC/prepare_pairwise_data.sh
@@ -19,18 +19,18 @@ cmd="python3 ${fmri_github_dir}/helper_functions/data_prep_and_QC/split_MTS_into
 --data_path ${data_path} \
 --noise_procs "$noise_procs_list" \
 --dataset_ID ${dataset_ID}"
-$cmd
+echo $cmd
 
-# Create sample.yaml file for this dataset
-for noise_proc in $noise_procs_list
-do
-    noise_label=$(echo $noise_proc | sed "s/\+/_/g")
-    cmd="Rscript $github_dir/pyspi-distribute/create_yaml_for_samples.R \
-    --data_dir ${data_path}/raw_data/numpy_files/${noise_label}/ \
-    --sample_metadata_file ${data_path}/${sample_metadata_file} \
-    --ID_var Sample_ID \
-    --label_vars Diagnosis \
-    --dim_order ps \
-    --overwrite"
-    $cmd
-done
+# # Create sample.yaml file for this dataset
+# for noise_proc in $noise_procs_list
+# do
+#     noise_label=$(echo $noise_proc | sed "s/\+/_/g")
+#     cmd="Rscript $github_dir/pyspi-distribute/create_yaml_for_samples.R \
+#     --data_dir ${data_path}/raw_data/numpy_files/${noise_label}/ \
+#     --sample_metadata_file ${data_path}/${sample_metadata_file} \
+#     --ID_var Sample_ID \
+#     --label_vars Diagnosis \
+#     --dim_order ps \
+#     --overwrite"
+#     $cmd
+# done
