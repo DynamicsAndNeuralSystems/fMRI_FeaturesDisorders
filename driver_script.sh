95f79b0762bb9480c39ea79776d498afc36b7907
Annie G. Bryant <anniegbryant@gmail.com>
2022-08-31 20:44:54 +1000

    parameterize pairwise data prep

--- driver_script.sh
+++ driver_script.sh
@@ -1,5 +1,5 @@
 ##########################################################################################
-export github_dir=/headnode1/abry4213/github/fMRI_FeaturesDisorders/
+export github_dir=/headnode1/abry4213/github/
 export univariate_feature_set="catch22"
 export pairwise_feature_set="pyspi14"
 
@@ -17,20 +17,24 @@ export noise_procs="AROMA+2P;AROMA+2P+GMR;AROMA+2P+DiCER"
 # export brain_region_lookup="Harvard_Oxford_cort_prob_2mm_ROI_lookup.csv"
 # export noise_procs="FC1000"
 
-cd $github_dir/data_prep_and_QC/
+cd $github_dir/fMRI_FeaturesDisorders/data_prep_and_QC/
 
 # Prep univariate data
-for run_number in 1 2 3 4 5; do
-  cmd="qsub -v run_number=$run_number,dataset_ID=$dataset_ID,univariate_feature_set=$univariate_feature_set,sample_metadata_file=$sample_metadata_file,brain_region_lookup=$brain_region_lookup,noise_procs=$noise_procs \
-  -N prepare_univariate_data_${dataset_ID}${run_number} \
-  -o /headnode1/abry4213/github/fMRI_FeaturesDisorders/cluster_output/prepare_univariate_data_${dataset_ID}${run_number}_out.txt \
-  -m a \
-  call_prepare_univariate_data.pbs"
-  echo $cmd
-  $cmd
-done
+# for run_number in 1 2 3 4 5; do
+#   qsub -v run_number=$run_number,dataset_ID=$dataset_ID,univariate_feature_set=$univariate_feature_set,sample_metadata_file=$sample_metadata_file,brain_region_lookup=$brain_region_lookup,noise_procs=$noise_procs \
+#   -N prepare_univariate_data_${dataset_ID}${run_number} \
+#   -o $github_dir/fMRI_FeaturesDisorders/cluster_output/prepare_univariate_data_${dataset_ID}${run_number}_out.txt \
+#   -m a \
+#   call_prepare_univariate_data.pbs
+# done
 
-# # Prep pairwise data
+# Prep pairwise data
+# Get data into .npy files
+qsub -N prepare_pairwise_data_${dataset_ID} -q yossarian -j oe \
+-v github_dir=$github_dir,data_path=$data_path,noise_procs=$noise_procs,dataset_ID=$dataset_ID,subject_metadata_file=$subject_metadata_file \
+-o $github_dir/fMRI_FeaturesDisorders/cluster_output/prepare_pairwise_data.txt \
+-l select=1:ncpus=1:mem=20GB -l walltime=4:00:00 -M abry4213@uni.sydney.edu.au -m a -V \
+data_prep_and_QC/prepare_pairwise_data.sh
 
 # # Get data into .npy files
 # qsub data_prep_and_QC/call_prepare_pairwise_data.pbs 
