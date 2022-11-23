201c151818baf69e85d94a0920aa7035af7199e4
anniegbryant <anniegbryant@gmail.com>
2022-11-24 05:43:14 +1100

    di_gaussian processing

--- data_prep_and_QC/pyspi_QC_analysis/SPI_troubleshooting.py
+++ data_prep_and_QC/pyspi_QC_analysis/SPI_troubleshooting.py
@@ -1,48 +1,36 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 
 import pandas as pd
 import numpy as np
+import os
+import dill
 from pyspi.calculator import Calculator
 from pyspi.data import Data
 
-
 # github path
-github_path = "/Users/abry4213/github/fMRI_FeaturesDisorders/data_visualisation/quality_control/"
-
+github_path = "/headnode1/abry4213/github/fMRI_FeaturesDisorders/"
 # Define data path
-SCZ_data_path = "/Users/abry4213/data/UCLA_Schizophrenia/raw_data/numpy_files/AROMA_2P_GMR/" 
+SCZ_data_path = "/headnode1/abry4213/data/UCLA_Schizophrenia/raw_data/numpy_files/AROMA_2P_GMR/" 
 
-subject = "sub-10171"
-subject_data = np.load(SCZ_data_path + subject + ".npy")
-
+############################# di_gaussian redo #################################
 # config file
-config_file = github_path + "pyspi_di_gaussian_config.yaml"
+config_file = github_path + "data_prep_and_QC/pyspi_QC_analysis/pyspi_di_gaussian_config.yaml"
 
-calc = Calculator(dataset=subject_data, configfile=config_file)
-calc.compute()
+subject_list = ["sub-10206", "sub-10292", "sub-10438", "sub-10624",
+                "sub-10674", "sub-11019", "sub-11066", "sub-11122",
+                "sub-11131", "sub-11143", "sub-50015", "sub-50016",
+                "sub-50022"]
 
-calc_res = calc.table
+for subject in subject_list:
+    if not os.path.exists(SCZ_data_path + subject + "/calc_di_gaussian.pkl"):
+        subject_data = np.load(SCZ_data_path + subject + ".npy")
+    
+        calc = Calculator(dataset=subject_data, configfile=config_file)
+        calc.compute()
+    
+        calc_res = calc.table
+    
+        # Save calc results to a pickle file
+        with open(SCZ_data_path + subject + "/calc_di_gaussian.pkl", 'wb') as f:
+            dill.dump(calc_res, f)
 
-
-# Iterate over each SPI
-calc_res.columns = calc_res.columns.to_flat_index()
-
-# Convert index to column
-calc_res.reset_index(level=0, inplace=True)
-
- # Rename index as first brain region
-calc_res = calc_res.rename(columns={"index": "brain_region_1"})
-
-# Pivot data from wide to long
-SPI_res_long = pd.melt(calc_res, id_vars="brain_region_1")
-SPI_res_long['SPI'], SPI_res_long['brain_region_2'] = SPI_res_long.variable.str
-
- # Remove variable column
-SPI_res_long = SPI_res_long.drop("variable", 1)
-
-SPI_res_long.to_csv("temp.csv")
