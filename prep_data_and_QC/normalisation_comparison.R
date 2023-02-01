c5039f6c9b0ab4624623ee7113030ead5c084aae
anniegbryant <anniegbryant@gmail.com>
2023-02-01 21:53:20 +1100

    Final normalisation analysis figures

--- prep_data_and_QC/normalisation_comparison.R
+++ prep_data_and_QC/normalisation_comparison.R
@@ -35,67 +35,50 @@ TAF::mkdir(plot_path)
 UCLA_CNP_data_path <- "~/data/UCLA_CNP/processed_data/"
 ABIDE_ASD_data_path <- "~/data/ABIDE_ASD/processed_data/"
 
-# Load metadata
-UCLA_CNP_metadata <- pyarrow_feather$read_feather("~/data/UCLA_CNP/study_metadata/UCLA_CNP_sample_metadata.feather")
-ABIDE_ASD_metadata <- pyarrow_feather$read_feather("~/data/ABIDE_ASD/study_metadata/ABIDE_ASD_sample_metadata.feather")
-
-# Load catch22 data
 UCLA_CNP_catch22_data <- pyarrow_feather$read_feather(glue("{UCLA_CNP_data_path}/UCLA_CNP_AROMA_2P_GMR_catch22_filtered.feather")) %>%
   mutate(Normalization = "Raw Data")
 ABIDE_ASD_catch22_data <- pyarrow_feather$read_feather(glue("{ABIDE_ASD_data_path}/ABIDE_ASD_FC1000_catch22_filtered.feather")) %>%
   mutate(Normalization = "Raw Data")
 
 # Apply normalization
-apply_transform_to_dataset <- function(dataset, metadata, transform_name) {
+apply_transform_to_dataset <- function(dataset, transform_name) {
   # Initialise list for transformed data results
   transformed_data_list <- list()
-  
-  # Combine with metadata
-  dataset <- left_join(dataset, metadata)
-  
-  # Iterate over each diagnostic group
-  for (dx_group in unique(dataset$Diagnosis)) {
-    dx_group_data <- subset(dataset, Diagnosis==dx_group)
+  for (brain_region in unique(dataset$Brain_Region)) {
+    region_data <- dataset %>%
+      filter(Brain_Region == brain_region) %>%
+      dplyr::select(Sample_ID, names, values) %>%
+      tidyr::pivot_wider(id_cols = Sample_ID, names_from = names, values_from = values)
     
-    # Iterate over each brain region
-    for (brain_region in unique(dx_group_data$Brain_Region)) {
-      region_data <- dx_group_data %>%
-        filter(Brain_Region == brain_region) %>%
-        dplyr::select(Sample_ID, names, values) %>%
-        tidyr::pivot_wider(id_cols = Sample_ID, names_from = names, values_from = values)
-      
-      subjects <- region_data$Sample_ID
-      
-      region_matrix <- region_data %>%
-        dplyr::select(-Sample_ID) %>%
-        as.matrix()
-      
-      TS_features <- colnames(region_matrix)
-      
-      # Instantiate the transformer
-      if (transform_name == "z-score") {
-        transformer <- StandardScaler()$fit(region_matrix)
-      } else {
-        transformer <- RobustSigmoidScaler(unit_variance=TRUE)$fit(region_matrix)
-      }
-      
-      # Apply transformer
-      region_data_trans <- as.data.frame(transformer$transform(region_matrix))
-      colnames(region_data_trans) <- TS_features
-      region_data_trans$Sample_ID <- subjects
-      
-      # Reshape from wide to long
-      region_data_trans_long <- region_data_trans %>%
-        pivot_longer(cols=c(-Sample_ID), names_to="names", values_to="values") %>%
-        mutate(Brain_Region = brain_region,
-               Diagnosis = dx_group)
-      
-      # Append region data to list
-      transformed_data_list <- list.append(transformed_data_list, 
-                                           region_data_trans_long)
+    subjects <- region_data$Sample_ID
+    
+    region_matrix <- region_data %>%
+      dplyr::select(-Sample_ID) %>%
+      as.matrix()
+    
+    TS_features <- colnames(region_matrix)
+    
+    # Instantiate the transformer
+    if (transform_name == "z-score") {
+      transformer <- StandardScaler()$fit(region_matrix)
+    } else {
+      transformer <- RobustSigmoidScaler(unit_variance=TRUE)$fit(region_matrix)
     }
+    
+    # Apply transformer
+    region_data_trans <- as.data.frame(transformer$transform(region_matrix))
+    colnames(region_data_trans) <- TS_features
+    region_data_trans$Sample_ID <- subjects
+    
+    # Reshape from wide to long
+    region_data_trans_long <- region_data_trans %>%
+      pivot_longer(cols=c(-Sample_ID), names_to="names", values_to="values") %>%
+      mutate(Brain_Region = brain_region)
+    
+    # Append region data to list
+    transformed_data_list <- list.append(transformed_data_list, 
+                                         region_data_trans_long)
   }
-  
   
   # Concatenate results along list
   transformed_data <- do.call(plyr::rbind.fill, transformed_data_list)
@@ -105,30 +88,24 @@ apply_transform_to_dataset <- function(dataset, metadata, transform_name) {
 
 # z-score
 UCLA_CNP_catch22_data_zscore <- apply_transform_to_dataset(UCLA_CNP_catch22_data,
-                                                           UCLA_CNP_metadata,
                                                            "z-score") %>%
   mutate(Normalization = "z-score")
 
 ABIDE_ASD_catch22_data_zscore <- apply_transform_to_dataset(ABIDE_ASD_catch22_data,
-                                                            ABIDE_ASD_metadata,
                                                             "z-score") %>%
   mutate(Normalization = "z-score")
 
 # robust sigmoid
 UCLA_CNP_catch22_data_robust_sigmoid <- apply_transform_to_dataset(UCLA_CNP_catch22_data,
-                                                                   UCLA_CNP_metadata,
                                                                    "RobustSigmoid") %>%
   mutate(Normalization = "Robust Sigmoid")
 ABIDE_ASD_catch22_data_robust_sigmoid <- apply_transform_to_dataset(ABIDE_ASD_catch22_data,
-                                                                    ABIDE_ASD_metadata,
                                                                     "RobustSigmoid") %>%
   mutate(Normalization = "Robust Sigmoid")
 
 # Function to plot raw data, z-scored data, and robust sigmoid-transformed data for each catch22 featuree
-plot_values <- function(feature_data, dx_group, metadata, norm_type="none", y_label="Raw\nValues") {
+plot_values <- function(feature_data, norm_type="none", y_label="Raw\nValues") {
   p <- feature_data %>%
-    left_join(., metadata) %>%
-    filter(Diagnosis == dx_group) %>%
     mutate(names = gsub("_", " ", names)) %>%
     ggplot(data = ., mapping = aes(x = values,
                                    y = after_stat(count)/sum(after_stat(count)), 
@@ -158,24 +135,19 @@ plot_values <- function(feature_data, dx_group, metadata, norm_type="none", y_la
 }
 
 # Plot values for UCLA CNP
-for (dx in c("Schizophrenia", "ADHD", "Bipolar", "Control")) {
-  UCLA_CNP_dx_plot_list <- list(plot_values(UCLA_CNP_catch22_data, dx_group = dx, metadata = UCLA_CNP_metadata, norm_type="none", "Raw\nValues"),
-                                plot_values(UCLA_CNP_catch22_data_zscore, dx_group = dx, metadata = UCLA_CNP_metadata, norm_type="z_score", "z-score"),
-                                plot_values(UCLA_CNP_catch22_data_robust_sigmoid, dx_group = dx, metadata = UCLA_CNP_metadata, norm_type="robust_sigmoid", "Robust\nSigmoid"))
-  
-  wrap_plots(UCLA_CNP_dx_plot_list, ncol=1, heights=c(0.36, 0.3, 0.3))
-  ggsave(glue("{plot_path}/UCLA_CNP_catch22_norms_{dx}.png"), bg="white",
-         width = 28, height = 5, units = "in", dpi = 300)
-}
+UCLA_CNP_plot_list <- list(plot_values(UCLA_CNP_catch22_data, norm_type="none", "Raw\nValues"),
+                       plot_values(UCLA_CNP_catch22_data_zscore, norm_type="z_score", "z-score"),
+                       plot_values(UCLA_CNP_catch22_data_robust_sigmoid, norm_type="robust_sigmoid", "Robust\nSigmoid"))
 
+wrap_plots(UCLA_CNP_plot_list, ncol=1, heights=c(0.36, 0.3, 0.3))
+ggsave(glue("{plot_path}/UCLA_CNP_catch22_norms.png"), bg="white",
+       width = 28, height = 5, units = "in", dpi = 300)
 
 # Plot values for ABIDE ASD
-for (dx in c("ASD", "Control")) {
-  ABIDE_ASD_dx_plot_list <- list(plot_values(ABIDE_ASD_catch22_data, dx_group = dx, metadata = ABIDE_ASD_metadata, norm_type="none", "Raw\nValues"),
-                              plot_values(ABIDE_ASD_catch22_data_zscore, dx_group = dx, metadata = ABIDE_ASD_metadata, norm_type="z_score", "z-score"),
-                              plot_values(ABIDE_ASD_catch22_data_robust_sigmoid, dx_group = dx, metadata = ABIDE_ASD_metadata, norm_type="robust_sigmoid", "Robust\nSigmoid"))
-  
-  wrap_plots(ABIDE_ASD_dx_plot_list, ncol=1, heights=c(0.36, 0.3, 0.3))
-  ggsave(glue("{plot_path}/ABIDE_ASD_catch22_norms.png"), bg="white",
-         width = 28, height = 5, units = "in", dpi = 300)
-}
+ABIDE_ASD_plot_list <- list(plot_values(ABIDE_ASD_catch22_data, norm_type="none", "Raw\nValues"),
+                           plot_values(ABIDE_ASD_catch22_data_zscore, norm_type="z_score", "z-score"),
+                           plot_values(ABIDE_ASD_catch22_data_robust_sigmoid, norm_type="robust_sigmoid", "Robust\nSigmoid"))
+
+wrap_plots(ABIDE_ASD_plot_list, ncol=1, heights=c(0.36, 0.3, 0.3))
+ggsave(glue("{plot_path}/ABIDE_ASD_catch22_norms.png"), bg="white",
+       width = 28, height = 5, units = "in", dpi = 300)
