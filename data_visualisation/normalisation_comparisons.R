2a217eb959a338d15837c76a94734296d1c45f43
anniegbryant <anniegbryant@gmail.com>
2023-08-29 10:42:25 +1000

    normalisation comparison analysis

--- data_visualisation/normalisation_comparisons.R
+++ data_visualisation/normalisation_comparisons.R
@@ -33,7 +33,8 @@ UCLA_CNP_data_path <- "~/data/UCLA_CNP/processed_data/"
 ABIDE_ASD_data_path <- "~/data/ABIDE_ASD/processed_data/"
 
 # Load feature info data
-TS_feature_info <- read.csv(glue("{github_dir}/data_visualisation/catch24_info.csv"))
+catch24_feature_info <- read.csv("~/github/fMRI_FeaturesDisorders/data_visualisation/catch24_info.csv")
+pyspi14_feature_info <- read.csv("~/github/fMRI_FeaturesDisorders/data_visualisation/SPI_info.csv")
 
 # Load univariate catch24 data
 UCLA_CNP_catch24_raw_data <- pyarrow_feather$read_feather(glue("{UCLA_CNP_data_path}/UCLA_CNP_AROMA_2P_GMR_catch24_filtered.feather")) %>%
@@ -80,16 +81,16 @@ ABIDE_ASD_pyspi14_RS <- pyarrow_feather$read_feather(glue("{ABIDE_ASD_data_path}
 
 # Plot values for UCLA CNP
 # Function to plot raw data, z-scored data, and mixed sigmoid-transformed data for each catch24 featuree
-plot_values <- function(feature_data, norm_type="none", y_label="Raw\nValues") {
+plot_values <- function(feature_data, TS_feature_info, norm_type="none", y_label="Raw\nValues") {
   p <- feature_data %>%
-    dplyr::rename("TS_Feature" = "names") %>%
     left_join(., TS_feature_info) %>%
+    mutate(Figure_name = gsub("_", " ", Figure_name)) %>%
     ggplot(data = ., mapping = aes(x = values,
                                    y = after_stat(count)/sum(after_stat(count)), 
-                                   fill = Figure_Name)) +
+                                   fill = Figure_name)) +
     geom_histogram() +
-    facet_wrap(Figure_Name ~ ., scales="free", nrow=1,
-               labeller = labeller(Figure_Name = label_wrap_gen(10))) +
+    facet_wrap(Figure_name ~ ., scales="free", nrow=1,
+               labeller = labeller(Figure_name = label_wrap_gen(10))) +
     ylab(y_label) +
     theme(legend.position = "none",
           axis.title.x = element_blank(),
@@ -112,18 +113,18 @@ plot_values <- function(feature_data, norm_type="none", y_label="Raw\nValues") {
 }
 
 # catch24
-UCLA_CNP_catch24_plot_list <- list(plot_values(UCLA_CNP_catch24_raw_data, norm_type="none", "Raw\nValues"),
-                                   plot_values(UCLA_CNP_catch24_z, norm_type="z_score", "z-score"),
-                                   plot_values(UCLA_CNP_catch24_RS, norm_type="mixed_sigmoid", "Mixed\nSigmoid"))
+UCLA_CNP_catch24_plot_list <- list(plot_values(feature_data=UCLA_CNP_catch24_raw_data %>% dplyr::rename("catch24_name" = "names"), TS_feature_info=catch24_feature_info, norm_type="none", "Raw\nValues"),
+                                   plot_values(UCLA_CNP_catch24_z %>% dplyr::rename("catch24_name" = "names"), TS_feature_info=catch24_feature_info, norm_type="z_score", "z-score"),
+                                   plot_values(UCLA_CNP_catch24_RS %>% dplyr::rename("catch24_name" = "names"), TS_feature_info=catch24_feature_info, norm_type="mixed_sigmoid", "Mixed\nSigmoid"))
 
 wrap_plots(UCLA_CNP_catch24_plot_list, ncol=1, heights=c(0.36, 0.3, 0.3))
 ggsave(glue("{plot_path}/UCLA_CNP_catch24_norms.svg"), bg="white",
        width = 28, height = 5, units = "in", dpi = 300)
 
 # pyspi14
-UCLA_CNP_pyspi14_plot_list <- list(plot_values(UCLA_CNP_pyspi14_raw_data, norm_type="none", "Raw\nValues"),
-                                   plot_values(UCLA_CNP_pyspi14_z, norm_type="z_score", "z-score"),
-                                   plot_values(UCLA_CNP_pyspi14_RS, norm_type="mixed_sigmoid", "Mixed\nSigmoid"))
+UCLA_CNP_pyspi14_plot_list <- list(plot_values(UCLA_CNP_pyspi14_raw_data %>% dplyr::rename("pyspi_name" = "names"), TS_feature_info=pyspi14_feature_info, norm_type="none", "Raw\nValues"),
+                                   plot_values(UCLA_CNP_pyspi14_z %>% dplyr::rename("pyspi_name" = "names"), TS_feature_info=pyspi14_feature_info, norm_type="z_score", "z-score"),
+                                   plot_values(UCLA_CNP_pyspi14_RS %>% dplyr::rename("pyspi_name" = "names"), TS_feature_info=pyspi14_feature_info, norm_type="mixed_sigmoid", "Mixed\nSigmoid"))
 
 wrap_plots(UCLA_CNP_pyspi14_plot_list, ncol=1, heights=c(0.36, 0.3, 0.3))
 ggsave(glue("{plot_path}/UCLA_CNP_pyspi14_norms.svg"), bg="white",
@@ -132,18 +133,18 @@ ggsave(glue("{plot_path}/UCLA_CNP_pyspi14_norms.svg"), bg="white",
 # Plot values for ABIDE ASD
 
 # catch24
-ABIDE_ASD_catch24_plot_list <- list(plot_values(ABIDE_ASD_catch24_raw_data, norm_type="none", "Raw\nValues"),
-                                    plot_values(ABIDE_ASD_catch24_z, norm_type="z_score", "z-score"),
-                                    plot_values(ABIDE_ASD_catch24_RS, norm_type="mixed_sigmoid", "Mixed\nSigmoid"))
+ABIDE_ASD_catch24_plot_list <- list(plot_values(ABIDE_ASD_catch24_raw_data %>% dplyr::rename("catch24_name" = "names"), TS_feature_info=catch24_feature_info, norm_type="none", "Raw\nValues"),
+                                    plot_values(ABIDE_ASD_catch24_z %>% dplyr::rename("catch24_name" = "names"), TS_feature_info=catch24_feature_info, norm_type="z_score", "z-score"),
+                                    plot_values(ABIDE_ASD_catch24_RS %>% dplyr::rename("catch24_name" = "names"), TS_feature_info=catch24_feature_info, norm_type="mixed_sigmoid", "Mixed\nSigmoid"))
 
 wrap_plots(ABIDE_ASD_catch24_plot_list, ncol=1, heights=c(0.36, 0.3, 0.3))
 ggsave(glue("{plot_path}/ABIDE_ASD_catch24_norms.svg"), bg="white",
        width = 28, height = 5, units = "in", dpi = 300)
 
 # pyspi14
-ABIDE_ASD_pyspi14_plot_list <- list(plot_values(ABIDE_ASD_pyspi14_raw_data, norm_type="none", "Raw\nValues"),
-                                    plot_values(ABIDE_ASD_pyspi14_z, norm_type="z_score", "z-score"),
-                                    plot_values(ABIDE_ASD_pyspi14_RS, norm_type="mixed_sigmoid", "Mixed\nSigmoid"))
+ABIDE_ASD_pyspi14_plot_list <- list(plot_values(ABIDE_ASD_pyspi14_raw_data %>% dplyr::rename("pyspi_name" = "names"), TS_feature_info=pyspi14_feature_info, norm_type="none", "Raw\nValues"),
+                                    plot_values(ABIDE_ASD_pyspi14_z %>% dplyr::rename("pyspi_name" = "names"), TS_feature_info=pyspi14_feature_info, norm_type="z_score", "z-score"),
+                                    plot_values(ABIDE_ASD_pyspi14_RS %>% dplyr::rename("pyspi_name" = "names"), TS_feature_info=pyspi14_feature_info, norm_type="mixed_sigmoid", "Mixed\nSigmoid"))
 
 wrap_plots(ABIDE_ASD_pyspi14_plot_list, ncol=1, heights=c(0.36, 0.3, 0.3))
 ggsave(glue("{plot_path}/ABIDE_ASD_pyspi14_norms.svg"), bg="white",
