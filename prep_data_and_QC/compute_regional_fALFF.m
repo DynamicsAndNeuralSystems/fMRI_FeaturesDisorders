f8bc41bdc409acf7f2c589deb5637738b89b86f6
anniegbryant <anniegbryant@gmail.com>
2023-09-03 19:33:08 +1000

    Compute regional ALFF and fALFF

--- prep_data_and_QC/compute_regional_fALFF.m
+++ prep_data_and_QC/compute_regional_fALFF.m
@@ -1,10 +1,12 @@
-UCLA_CNP_data_path = '/Users/abry4213/data/UCLA_CNP/raw_data/';
-ABIDE_ASD_data_path = '/Users/abry4213/data/ABIDE_ASD/raw_data/';
+UCLA_CNP_data_path = '/Users/abry4213/data/UCLA_CNP/';
+ABIDE_ASD_data_path = '/Users/abry4213/data/ABIDE_ASD/';
+
+% Define TR as 2s
+samplingPeriod = 2;
 
 % Load in feather data file
 UCLA_CNP_TS_file = "UCLA_CNP_AROMA_2P_GMR_fMRI_TS.feather";
 
-%%
 % Path to the python script that converts the .feather file to .mat if
 % needed
 python_script_path = "/Users/abry4213/Library/CloudStorage/OneDrive-TheUniversityofSydney(Students)/github/fMRI_FeaturesDisorders/prep_data_and_QC/feather_to_mat.py";
@@ -18,21 +20,27 @@ result
 % Load the .mat file
 load(sprintf('%s.mat', feather_file_base));
 
-%% 
+[m, n] = size(feather_to_mat_struct.Sample_ID);
+% Initialise fALFF results
+fALFF_data = cell(m,3);
 
-ALFF_data = cell(size(TimeSeries,1),3);
-samplingPeriod = 2.68;
+for i = 1:m
+    sample_ID = feather_to_mat_struct.Sample_ID(i, :); % Extract sample ID
+    brain_region = feather_to_mat_struct.Brain_Region(i, :); % Extract brain region
+    time_series = transpose(feather_to_mat_struct.Time_Series(i, :)); % Extract time series
 
-for i = 1:size(TimeSeries,1)
-    TS_sample = TimeSeries.Name{i};
-    TS_data = TimeSeries.Data{i};
+    % Compute fALFF for this participant/region
+    SP_fALFF_res = SP_fALFF(time_series, samplingPeriod);
+    
+    fALFF = SP_fALFF_res.fALFF;
+    ALFF = SP_fALFF_res.ALFF;
 
-    % Ben's implementation of fALFF in Fallon et al 2020
-    fALFF = SP_fALFF(TS_data, samplingPeriod);
-    % Append to ALFF_data
-    ALFF_data{i, 1} = TS_sample;
-    ALFF_data{i, 2} = fALFF.ALFF;
-    ALFF_data{i, 3} = fALFF.fALFF;
+    % Add results to mat
+    fALFF_data{i, 1} = sample_ID;
+    fALFF_data{i, 2} = brain_region;
+    fALFF_data{i, 3} = fALFF;
+    fALFF_data{i, 4} = ALFF;
 end
 
-save(strcat(data_path, 'All_rsfMRI_data_ALFF_fALFF.mat'), 'ALFF_data', '-v6')
+
+save(strcat(UCLA_CNP_data_path, 'processed_data/UCLA_CNP_AROMA_2P_GMR_ALFF_fALFF.mat'), 'fALFF_data', '-v6')
